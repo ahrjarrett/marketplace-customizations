e12abbf0c410e17db30d6fd2b40a78a4028260c2
Andrew Jarrett <ahrjarrett@gmail.com>
2018-05-31 17:18:57 -0500

    remove wrapper conditional, put mutations at end of file

--- scripts.js
+++ scripts.js
@@ -1,42 +1,42 @@
 
-if (window.location.host === 'admin.austin.ownlocal.com') {
 
-  /*** PATH STUFF: ***/
-  let { pathname } = window.location
-  let businessPath = /\/businesses\/\d+$/
-  let logoPath = /\/businesses\/\d+\/logo/
+/*** PATH STUFF: ***/
+let { pathname } = window.location
+let businessPath = /\/businesses\/\d+$/
+let logoPath = /\/businesses\/\d+\/logo/
 
-  /*** HELPER FUNCTIONS: ***/
-  let firstChild = el => el.childNodes[0]
-  let testPathname = pathname => re => re.test(pathname)
-  // testPath :: Regex -> Bool
-  let testPath = testPathname(pathname)
+/*** HELPER FUNCTIONS: ***/
+let firstChild = el => el.childNodes[0]
+let testPathname = pathname => re => re.test(pathname)
+// testPath :: Regex -> Bool
+let testPath = testPathname(pathname)
     
-  let applyBusinessPathStyles = () => {
-    let mainContent = document.getElementById('content')
-    let leftCol = document.querySelector('.left-col')
-    let profileSettingsh4 = leftCol.childNodes[7]
-    let profileSettingsContent = leftCol.childNodes[9]
-    let editUrl = profileSettingsContent
-	  .childNodes[1]
-	  .childNodes[0]
-	  .href
+let applyBusinessPathStyles = () => {
+  let mainContent = document.getElementById('content')
+  let leftCol = document.querySelector('.left-col')
+  let profileSettingsh4 = leftCol.childNodes[7]
+  let profileSettingsContent = leftCol.childNodes[9]
+  let editUrl = profileSettingsContent
+      .childNodes[1]
+      .childNodes[0]
+      .href
 			
-    let editDiv = document.createElement('a')
-    editDiv.text = 'Edit Business'
-    editDiv.id = 'edit_div_custom'
-    editDiv.href = editUrl
-    mainContent.prepend(editDiv)
-  }
+  let editDiv = document.createElement('a')
+  editDiv.text = 'Edit Business'
+  editDiv.id = 'edit_div_custom'
+  editDiv.href = editUrl
+  mainContent.prepend(editDiv)
+}
     
 
-  let applyLogoPathStyles = () => {
-    let mpLogo = document.querySelector('.edit_business')
-	.childNodes[5].childNodes[0]
+let applyLogoPathStyles = () => {
+  let mpLogo = document.querySelector('.edit_business')
+      .childNodes[5].childNodes[0]
 
-    mpLogo.style.border = '1px solid lightgrey'
-  }
+  mpLogo.style.border = '1px solid lightgrey'
+}
     
+if (window.location.host === 'admin.austin.ownlocal.com') {
   if (testPath(businessPath)) {
     applyBusinessPathStyles()
   }
